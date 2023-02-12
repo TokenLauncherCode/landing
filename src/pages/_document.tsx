7258cdf31278e1c754eaff7866535d6c6e5149eb
Sean Luther <elmutt@gmail.com>
2023-02-12 13:58:47 -0700

    test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import { Html, Head, Main, NextScript } from 'next/document'
-import { useEffect } from 'react'
 
 export default function Document() {
   return (
