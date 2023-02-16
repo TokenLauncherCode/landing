4b8ab9e86c9f0704c6f00d68fe0404a1568b3d04
Sean Luther <elmutt@gmail.com>
2023-02-15 21:13:36 -0700

    remove unnecessary code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,7 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  const theme = extendTheme({
-    styles: {
-      global: () => ({
-        body: {
-          bg: "#222230",
-        },
-      }),
-    },
-  });
+  const theme = extendTheme({});
   
   return (
     <ChakraProvider theme={theme}>
